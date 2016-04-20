81ecbb2b01e87e60537299e6b0fe12b5609d5c4a
Luca Capra <luca.capra@gmail.com>
2016-04-20 15:46:58 +0200

    up

--- example.js
+++ example.js
@@ -1,38 +1,60 @@
 var Tag = require('./index');
 
-// var tag = new Tag('myobject');
-//
-// var child1 = tag.add({
-//   name: 'test1',
-//   description: ['some']
-// });
-//
-// console.log(require('util').inspect(tag.toJSON(), { depth: null }));
+var tag = new Tag('myobject');
+
+var child1 = tag.add({
+  name: 'test1',
+  description: ['some']
+});
+
+console.log(require('util').inspect(tag.toJSON(), { depth: null }));
 
 var tag = new Tag('root');
 
 var add100 = function (tag1, depth) {
-
     depth = depth || 0;
     depth++;
-
-    // console.log("add depth " + depth);
-
     for(var i = 0; i < 5; i++) {
-        var child = tag1.add('newtag_' + i);
+        var child = tag1.add(null, 'newtag_' + i);
         if(depth < 5) {
           add100(child, depth);
         }
     }
-
 };
 
-add100(tag);
+// add100(tag);
+// console.log(Tag.fromJSON(tag.toJSON()).toJSON());
+
+var tag = Tag.fromJSON({
+  id: 'colors',
+  children: [
+    {
+      id: 'red',
+      children: [
+        {
+          id: 'orange',
+          children: [
+            {
+              id: 'yellow',
+            }
+          ]
+        },
+      ]
+    },
+    {
+      id: 'blue',
+    },
+    {
+      id: 'green',
+    }
+  ]
+});
 
 // console.log(tag.toJSON());
+console.log(tag.getById('yellow'));
+console.log(tag.getById('yellow').toString());
 
-console.log(
-  require('util').inspect(
-    Tag.fromJSON(tag.toJSON()).toJSON(),
-  { depth: null })
-);
+console.log(tag.getById('yellow').toString());
+tag.getById('orange').remove('yellow');
+console.log(tag.getById('yellow') === null);
+console.log(tag.getRoot().path());
--- index.js
+++ index.js
@@ -9,129 +9,179 @@ function clone(obj) {
     return copy;
 }
 
-var Tag = function (id, info, parent) {
+var Tag = function (id, data, parent) {
 
     var tag = this;
-    tag.id = null;
 
-    parent = parent || null;
-
-    if(!parent && info instanceof Tag) {
-        parent = info;
-        info = id;
+    if(!parent && data instanceof Tag) {
+        parent = data;
+        data = id;
         id = null;
     }
 
-    var depth = parent ? parent.getDepth() + 1 : 0;
-
-    var root = parent || tag;
-    var children = [];
+    tag.id = null;
+    tag.data = null;
+    tag.parent = parent || null;
+    tag.depth = parent ? parent.getDepth() + 1 : 0;
+    tag.root = parent ? parent.getRoot() : tag;
+    tag.children = [];
+    this.data = data || id;
 
     // is root tag
     if(!parent) {
         tag.count = 1;
+        tag.tree = {};
     } else {
-        parent.getRoot().count++;
+        tag.parent.getRoot().count++;
     }
 
     // set defaults args
-    if(info === undefined) {
-        info = id;
-        id = null;
+    if(data === undefined) {
+        data = id;
+        if(typeof data !== 'string' && !tag.getById(data)) {
+          id = null;
+        }
     }
 
-    tag.id = id || 'tag' + (++lastTagId);
+    tag.id = id ? id : 'tag' + (++lastTagId);
 
-    tag.add = function (id, info) {
+    if(tag.getById(tag.id)) {
+        throw new Error('Id must be unique');
+    }
 
-        // handle list of tags
-        if(id instanceof Array) {
-            id.forEach(function (el) {
-                tag.add(el);
-            });
-            return null;
-        }
-
-
-        var child;
-
-        // import a Tag instance
-        if(id instanceof Tag) {
-          var prevTag = id;
-          child = new Tag(prevTag.id, prevTag.getTag(), tag);
-          if(prevTag.getChildren()) {
-            prevTag.getChildren().forEach(function(subel) {
-              child.add(subel);
-            });
-          }
-        }
-        else {
-
-          // handle an object
-          if(typeof id === 'object') {
-            return tag.add(id.id || null, id.tag || id);
-          }
-
-          child = new Tag(id, info, tag);
-        }
-
-        children.push(child);
-        return child;
-    };
-
-    tag.getParent = function () {
-        return parent;
-    };
-
-    tag.getChildren = function () {
-        return children;
-    };
-
-    tag.getRoot = function () {
-        return root;
-    };
-
-    tag.getDepth = function () {
-        return depth;
-    };
-
-    tag.getCount = function () {
-        return tag.getRoot().count;
-    };
-
-    tag.getTag = function () {
-        return info;
-    };
-
-    tag.toJSON = function () {
-
-        var json = {};
-
-        json.tag = tag.getTag() ? clone(tag.getTag()) : {};
-        json.id = tag.id;
-        json.depth = tag.getDepth();
-
-        // for root element only
-        if(tag.count) {
-            json.count = tag.count;
-        }
-
-        json.leaf = tag.getParent() ? true : false;
-        json.children = tag.getChildren().map(function (child) {
-            return child.toJSON();
-        });
-
-        return json;
-    };
-
+    // add root id to list
+    if(!parent) {
+        tag.tree[tag.id] = tag;
+    }
 };
 
+Tag.prototype.add = function (id, data) {
+    var tag = this;
+    // handle list of tags
+    if(id instanceof Array) {
+        id.forEach(function (el) {
+            tag.add(el);
+        });
+        return null;
+    }
+
+    var child;
+    // import a Tag instance
+    if(id instanceof Tag) {
+        var prevTag = id;
+        child = new Tag(prevTag.id, prevTag.getTag(), tag);
+        if(prevTag.getChildren()) {
+            prevTag.getChildren().forEach(function (subel) {
+                child.add(subel);
+            });
+        }
+    } else {
+        // handle an object
+        if(id && typeof id === 'object') {
+            return tag.add(id.id || null, id.tag || null);
+        }
+        child = new Tag(id, data, tag);
+    }
+
+    tag.getRoot().tree[child.id] = child;
+    tag.getChildren().push(child);
+    return child;
+};
+
+Tag.prototype.remove = function (tagchild) {
+    var tag = this;
+    var tagchildId = tagchild instanceof Tag ?  tagchild.id : tagchild;
+    tag.getChildren().forEach(function (child, idx) {
+        if(tagchildId === child.id) {
+            child.parent = null;
+            delete tag.getRoot().tree[tagchildId];
+            tag.getChildren().slice(idx, 1);
+        }
+    });
+};
+
+Tag.prototype.drop = function () {
+    var tag = this;
+    if(tag.getParent()) {
+        tag.getParent().remove(tag);
+    }
+};
+
+Tag.prototype.path = function (showIds) {
+    showIds = showIds === undefined ? false : showIds;
+    var tag = this;
+    var currtag = tag;
+    var path = [ tag[ showIds ? 'id' : 'data' ] ];
+    while(currtag.getParent()) {
+        currtag = currtag.getParent();
+        path.push(currtag[ showIds ? 'id' : 'data' ].toString());
+    }
+    return path.reverse().join('/');
+};
+
+Tag.prototype.getParent = function () {
+    return this.parent;
+};
+
+Tag.prototype.getChildren = function () {
+    return this.children;
+};
+
+Tag.prototype.getRoot = function () {
+    return this.root;
+};
+
+Tag.prototype.getDepth = function () {
+    return this.depth;
+};
+
+Tag.prototype.getCount = function () {
+    return this.getRoot().count;
+};
+
+Tag.prototype.getTag = function () {
+    return this.data;
+};
+
+Tag.prototype.getById = function (id) {
+    return (this.getRoot() && this.getRoot().tree[id]) ? this.getRoot().tree[id] : null;
+};
+
+Tag.prototype.toString = function () {
+  return this.path();
+};
+
+Tag.prototype.toJSON = function () {
+
+    var tag = this;
+    var json = {};
+
+    json.tag = tag.getTag() ? clone(tag.getTag()) : {};
+    json.id = tag.id;
+    json.depth = tag.getDepth();
+
+    // for root element only
+    if(tag.count) {
+        json.count = tag.count;
+    }
+
+    json.leaf = tag.getParent() ? true : false;
+    json.children = tag.getChildren().map(function (child) {
+        return child.toJSON();
+    });
+
+    return json;
+};
+
+
 var fromJSON = function (json, parent) {
-    var tag = new Tag(json.id || null, json.tag, parent || null);
+    var tag = !parent ?
+      new Tag(json.id, json.tag) : parent.add(json);
+
     if(json.children) {
-        tag.add(json.children.map(function (child) {
-            return fromJSON(child, tag);
-        }));
+        json.children.forEach(function(child) {
+          fromJSON(child, tag);
+        });
     }
     return tag;
 };
